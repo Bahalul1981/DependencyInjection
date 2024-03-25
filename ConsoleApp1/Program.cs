435f651df60b4c760ed54eac9f165d907905228f
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-03-25 19:31:52 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public interface IInjector
+    {
+        void PersonalInfo();
+    }
+
+    public class Bahalul : IInjector
+    {
+        public void PersonalInfo()
+        {
+            Console.WriteLine("My name is Dewan Bahalul");
+        }
+    }
+
+    public class Farhana : IInjector
+    {
+        public void PersonalInfo()
+        {
+            Console.WriteLine("My name is Farhana Yasmin");
+        }
+
+    }
+
+    public class Alvin : IInjector
+    {
+        public void PersonalInfo()
+        {
+            Console.WriteLine("My name is Alvin Dawn");
+        }
+
+    }
+
+    public class Client
+    {
+        readonly IInjector injector;
+        public Client(IInjector val)
+        {
+            this.injector = val;
+        }
+        public void GetValue()
+        {
+           injector.PersonalInfo();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IInjector bahalul = new Bahalul();
+            Client client = new Client(bahalul);
+            client.GetValue();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp2
+{
+
+
+    // Inhector Interface
+
+    public interface IInjector
+    {
+        string Name { get; }
+       
+    }
+
+    public class PersonOne : IInjector
+    {
+        public string Name { get; set; } = "Arvid Dawn Amin";
+
+    }
+    public class PersonTwo : IInjector
+    {
+        public string Name { get; set; } = "Farhana Yasmin";
+
+    }
+    public class PersonThree : IInjector
+    {
+        public string Name { get; set; } = "Alvin Dawn";
+
+    }
+
+    public class ClientClass
+    {
+        readonly IInjector injector;
+
+        public ClientClass(IInjector injector)
+        {
+            this.injector = injector;
+        }
+        public void GetValue () 
+        {
+            Console.WriteLine(injector.Name);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          IInjector enj= new PersonThree();
+
+          ClientClass clientClass = new ClientClass(enj);
+
+            clientClass.GetValue();
+
+           Console.ReadLine();
+        }
+    }
+}
